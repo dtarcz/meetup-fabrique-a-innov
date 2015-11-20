b39d01513e4722d62c04e846f56ee40093267b1e
simplon-davidt <dtarcz@gmail.com>
2015-11-20 10:04:35 +0100

    brainstorming1 sur la cible B

---
+++ fabrique-a-innov-cibleb.mm
@@ -0,0 +1,146 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="la-fabrique-a-innov-cible-b" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1448010097214"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      On cherche &#224; atteindre les Comp&#233;tences de cr&#233;ation : Designer, ing&#233;nieurs, desinateurs, graphistes, montage video
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="domaine des cr&#xe9;atifs" POSITION="right" ID="ID_1048910887" CREATED="1448007698671" MODIFIED="1448007705364">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="comment les contacter" POSITION="right" ID="ID_625415451" CREATED="1448007706046" MODIFIED="1448007719593">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="quels canaux?" POSITION="right" ID="ID_1270773319" CREATED="1448007738246" MODIFIED="1448007752163">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="quels outils ils utilisent ?" POSITION="right" ID="ID_1115277652" CREATED="1448007753520" MODIFIED="1448007764896">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="on cherche &#xe0; les atteindre par leur comp&#xe9;tence de conception" POSITION="right" ID="ID_1522930150" CREATED="1448007778719" MODIFIED="1448007797042">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="ils se l&#xe8;vent ils d&#xe9;jeunent" POSITION="left" ID="ID_1002564129" CREATED="1448007799565" MODIFIED="1448007823653">
+<edge COLOR="#ffff00"/>
+<node TEXT="messages sublininaux" ID="ID_1555230280" CREATED="1448007824999" MODIFIED="1448007831893"/>
+</node>
+<node TEXT="ils s&apos;inspirent de sites" POSITION="left" ID="ID_101501289" CREATED="1448007874762" MODIFIED="1448007882048">
+<edge COLOR="#7c0000"/>
+<node TEXT="exple images libres de droits" ID="ID_1552998374" CREATED="1448007882070" MODIFIED="1448007889817"/>
+<node TEXT="publicit&#xe9; sur ces sites ?" ID="ID_1015594543" CREATED="1448007890830" MODIFIED="1448007898898"/>
+</node>
+<node TEXT="c&apos;est des joueurs" POSITION="left" ID="ID_469318552" CREATED="1448007987992" MODIFIED="1448007993827">
+<edge COLOR="#00007c"/>
+<node TEXT="les attirer avec des jeux" ID="ID_1734155366" CREATED="1448007993848" MODIFIED="1448008000380"/>
+</node>
+<node TEXT="cr&#xe9;er un site de partage de ressources pour cr&#xe9;atifs" POSITION="left" ID="ID_561398304" CREATED="1448008021374" MODIFIED="1448008034268">
+<edge COLOR="#007c00"/>
+<node TEXT="antenne de la fabrique a innov" ID="ID_321584254" CREATED="1448008061136" MODIFIED="1448008074298"/>
+<node TEXT="forum" ID="ID_193196981" CREATED="1448008076330" MODIFIED="1448008079384"/>
+</node>
+<node TEXT="publicit&#xe9;" POSITION="left" ID="ID_624475627" CREATED="1448008127039" MODIFIED="1448008131875">
+<edge COLOR="#7c007c"/>
+<node TEXT="bandeau sur les forum" ID="ID_1117897970" CREATED="1448008131894" MODIFIED="1448008141539"/>
+</node>
+<node TEXT="des goodies" POSITION="right" ID="ID_1933642461" CREATED="1448008897187" MODIFIED="1448008902082">
+<edge COLOR="#007c7c"/>
+<node TEXT="mug" ID="ID_1673748019" CREATED="1448008902097" MODIFIED="1448008906631"/>
+<node TEXT="couteau suisse" ID="ID_679607200" CREATED="1448008909230" MODIFIED="1448008914924">
+<node TEXT="avec des outils utiles" ID="ID_552040839" CREATED="1448008914938" MODIFIED="1448008924374"/>
+</node>
+<node TEXT="cam&#xe9;ra appareil-phote BRIQUET" ID="ID_718109085" CREATED="1448008960975" MODIFIED="1448009004774">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node TEXT="envoie de mails" POSITION="left" ID="ID_1004919259" CREATED="1448009377736" MODIFIED="1448009383281">
+<edge COLOR="#7c7c00"/>
+<node TEXT="cr&#xe9;er une liste" ID="ID_566158561" CREATED="1448009402673" MODIFIED="1448009409096"/>
+<node TEXT="page d&apos;inscription" ID="ID_1461547242" CREATED="1448009409785" MODIFIED="1448009417113">
+<node TEXT="jeux" ID="ID_1116303608" CREATED="1448009425179" MODIFIED="1448009427667"/>
+<node TEXT="&#xe9;v&#xe8;nement" ID="ID_1652435905" CREATED="1448009428298" MODIFIED="1448009432627"/>
+</node>
+</node>
+<node TEXT="les toucher depuis des sites qu&apos;ils utilisent" POSITION="right" ID="ID_1885929508" CREATED="1448009532229" MODIFIED="1448009545218">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="messages avec le logo de la fabrique a innov" POSITION="right" ID="ID_981690160" CREATED="1448009560207" MODIFIED="1448009572952">
+<edge COLOR="#0000ff"/>
+<node TEXT="applications" ID="ID_774462216" CREATED="1448009590554" MODIFIED="1448009595665"/>
+<node TEXT="goodies" ID="ID_1775024873" CREATED="1448009596149" MODIFIED="1448009600190"/>
+</node>
+<node TEXT="toucher les gens sur les &#xe9;v&#xe8;nements" POSITION="left" ID="ID_1171402097" CREATED="1448009646816" MODIFIED="1448009656543">
+<edge COLOR="#00ff00"/>
+<node TEXT="forums de cr&#xe9;ation" ID="ID_1062668087" CREATED="1448009661525" MODIFIED="1448009668009"/>
+</node>
+<node TEXT="il y a des statistiques &#xe0; extraire" POSITION="left" ID="ID_163287271" CREATED="1448009673015" MODIFIED="1448009683557">
+<edge COLOR="#ff00ff"/>
+<node TEXT="quels sont leurs loisirs" ID="ID_646020655" CREATED="1448009685178" MODIFIED="1448009691696"/>
+<node TEXT="quels sont leurs sports" ID="ID_849390757" CREATED="1448009693129" MODIFIED="1448009700197"/>
+<node TEXT="quels sont leurs centres d&apos;int&#xe9;r&#xea;ts" ID="ID_1315853441" CREATED="1448009706645" MODIFIED="1448009716602"/>
+<node TEXT="sortir de leurs canaux habituels" ID="ID_1604741677" CREATED="1448009730042" MODIFIED="1448009740377"/>
+<node TEXT="qu&apos;est-ce qu&apos;ils consomment" ID="ID_1457484274" CREATED="1448009746547" MODIFIED="1448009753914"/>
+</node>
+<node TEXT="all&#xe9;ger les processus cr&#xe9;atif" POSITION="right" ID="ID_1418212998" CREATED="1448009962609" MODIFIED="1448009978239">
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
